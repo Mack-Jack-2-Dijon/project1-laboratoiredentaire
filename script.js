9dbd6d340bbc3ead65b68b1ee074e7a4164dbbff
FIRST\_NAME LAST\_NAME <Mack-Jack-2-Dijon\jcqs.thm@gmail.com>
2022-11-22 12:17:18 +0100

    first push

---
+++ script.js
@@ -0,0 +1,9 @@
+// Menu
+let menuButton = document.querySelector('#menu-button');
+let menu = document.querySelector('#menu');
+
+// Masquage
+menuButton.addEventListener('click',function(){
+  menu.classList.toggle('show-menu');
+  menuButton.classList.toggle('close');
+});
